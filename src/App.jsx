9743ba23cb60e8831d3216a85cd9cc201626fd6d
ShivaTejaPatel <namshivateja@gmail.com>
2024-02-13 01:54:39 +0530

    Created Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import Profile from "./pages/Profile"
 import Signin from "./pages/Signin"
 import Signup from "./pages/Signup"
 import About from "./pages/About"
-
+import Header from "./components/Header"
 function App() {
 
 
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
