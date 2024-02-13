6ff1242db33914da871265d41a3d510051da62fc
ShivaTejaPatel <namshivateja@gmail.com>
2024-02-13 21:38:12 +0530

    Complete Sign In Page Functionality

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -50,7 +50,12 @@ export default function Signup() {
         className='border p-3 rounded-lg' id = 'email' onChange={handleChange}/>
         <input type='password' placeholder='password' 
         className='border p-3 rounded-lg' id = 'password' onChange={handleChange}/>
-        <button className='bg-slate-700 text-white p-3  rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign Up</button>
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Sign Up'}
+        </button>
       </form>
       <div className='flex gap-2  mt-5'>
         <p>Have an Account?</p>
