c1e335646ea86f0c8939fc392554231536759747
ShivaTejaPatel <namshivateja@gmail.com>
2024-02-13 15:38:59 +0530

    Created a Middleware Function to Handle Possible Errors

--- api/index.js
+++ api/index.js
@@ -8,11 +8,25 @@ dotenv.config();
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log('Connected to MongoDB');
-    }).catch((err) => {
-        console.log(err);
-})
+}).catch((err) => {
+    console.error('MongoDB connection error:', err);
+});
+
 app.use(express.json());
 
-app.listen(3000, () => console.log('Server running on port 3000'));
-
+// Define your routes before error handling middleware
 app.use('/api/auth', authRouter);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success: false,
+        status: statusCode,
+        message: message,
+    });
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
