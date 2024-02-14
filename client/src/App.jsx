999a34854a18dacccf3025ac643b9ab283c6dc50
ShivaTejaPatel <namshivateja@gmail.com>
2024-02-14 21:27:03 +0530

    Cretaed Profile route and authenticated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,16 +6,19 @@ import Signin from "./pages/Signin"
 import Signup from "./pages/Signup"
 import About from "./pages/About"
 import Header from "./components/Header"
+import PrivateRoute from './components/PrivateRoute';
 function App() {
   return (
     <BrowserRouter>
     <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/about" element={<About />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
      
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -33,7 +33,7 @@ export default function SignUp() {
       }
       setLoading(false);
       setError(null);
-      navigate('/sign-in');
+      navigate('/signin');
     } catch (error) {
       setLoading(false);
       setError(error.message);
